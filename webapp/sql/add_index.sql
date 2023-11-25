a1a3b88b73ad05c6e1fc9a148a6b7002ed774760
yyamada12 <12yacropolisy@gmail.com>
2023-11-25 08:28:30 +0000

    [96150] add indexes

--- webapp/sql/add_index.sql
+++ webapp/sql/add_index.sql
@@ -1,3 +1,5 @@
 CREATE INDEX idx_reactions_livestream_id ON reactions (livestream_id);
 CREATE INDEX idx_ng_words_user_id_livestream_id ON ng_words (user_id, livestream_id);
 CREATE INDEX idx_icons_user_id ON icons (user_id);
+CREATE INDEX idx_livestreams_user_id ON livestreams (user_id);
+CREATE INDEX idx_slots_start_at_end_at ON reservation_slots (start_at, end_at);
