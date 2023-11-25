56df9dcf64f81397e7f6b9b6030bdd3e0ef26c57
yyamada12 <12yacropolisy@gmail.com>
2023-11-25 07:46:35 +0000

    [73443] add index for icons

--- webapp/sql/add_index.sql
+++ webapp/sql/add_index.sql
@@ -1,2 +1,3 @@
 CREATE INDEX idx_reactions_livestream_id ON reactions (livestream_id);
 CREATE INDEX idx_ng_words_user_id_livestream_id ON ng_words (user_id, livestream_id);
+CREATE INDEX idx_icons_user_id ON icons (user_id);
