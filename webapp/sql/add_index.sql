cc8347841b98aa7b20576546e13b451e31f2c87a
yyamada12 <12yacropolisy@gmail.com>
2023-12-03 13:41:15 +0000

    [145241] change index for reactions, add create_at desc

--- webapp/sql/add_index.sql
+++ webapp/sql/add_index.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_reactions_livestream_id ON reactions (livestream_id);
+CREATE INDEX idx_reactions_livestream_id_created_at ON reactions (livestream_id, created_at DESC);
 CREATE INDEX idx_ng_words_user_id_livestream_id ON ng_words (user_id, livestream_id);
 CREATE INDEX idx_icons_user_id ON icons (user_id);
 CREATE INDEX idx_livestreams_user_id ON livestreams (user_id);
