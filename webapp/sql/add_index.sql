e8f7332cbad0d0be536d5af19ef5d28ddb4d689e
yyamada12 <12yacropolisy@gmail.com>
2023-12-03 13:26:14 +0000

    [108940] add index for livecomments

--- webapp/sql/add_index.sql
+++ webapp/sql/add_index.sql
@@ -3,3 +3,4 @@ CREATE INDEX idx_ng_words_user_id_livestream_id ON ng_words (user_id, livestream
 CREATE INDEX idx_icons_user_id ON icons (user_id);
 CREATE INDEX idx_livestreams_user_id ON livestreams (user_id);
 CREATE INDEX idx_slots_start_at_end_at ON reservation_slots (start_at, end_at);
+CREATE INDEX idx_livecomments_livestream_id_created_at ON livecomments (livestream_id, created_at DESC);
